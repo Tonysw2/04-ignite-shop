6ccfdb44b924f107c231fb491f96afbf016a1a68
Tonysw2 <anthonyrmataa@gmail.com>
2022-12-16 22:45:03 -0300

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import type { AppProps } from 'next/app'
+import Image from 'next/image'
+import LogoImg from '../assets/Logo.svg'
+import { globalStyles } from '../styles/global'
+import { Container, Header } from '../styles/pages/app'
+
+globalStyles()
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image src={LogoImg} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
+}
---
+++ src/pages/product/[id].tsx
@@ -0,0 +1,100 @@
+import axios from 'axios'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import Image from 'next/image'
+import { useState } from 'react'
+import Stripe from 'stripe'
+import { stripe } from '../../lib/stripe'
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '../../styles/pages/product'
+
+interface ProductProps {
+  product: {
+    id: string
+    name: string
+    imageUrl: string
+    price: number
+    description: string
+    defaultPriceId: string
+  }
+}
+
+export default function Product({ product }: ProductProps) {
+  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] =
+    useState(false)
+
+  async function handleBuyProduct() {
+    console.log(product.defaultPriceId)
+    try {
+      setIsCreatingCheckoutSession(true)
+      const response = await axios.post('/api/checkout', {
+        priceId: product.defaultPriceId,
+      })
+
+      const { checkoutUrl } = response.data
+
+      window.location.href = checkoutUrl
+    } catch (error) {
+      alert('Falha ao redirectionar checkout!')
+    } finally {
+      setIsCreatingCheckoutSession(false)
+    }
+  }
+
+  return (
+    <ProductContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} height={520} width={480} alt="" />
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
+
+        <p>{product.description}</p>
+
+        <button disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}) => {
+  const productId = params!.id
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price'],
+  })
+
+  const price = product.default_price as Stripe.Price
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(price.unit_amount! / 100),
+        description: product.description,
+        defaultPriceId: price.id,
+      },
+    },
+    revalidate: 60 * 60 * 1, // 1 hour
+  }
+}
