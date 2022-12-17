c04a28cbffd7239436c45a93bda1d2f27df78972
Tonysw2 <anthonyrmataa@gmail.com>
2022-12-17 02:31:47 -0300

    Improve SEO with the Head Nextjs component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
 import Image from 'next/image'
+import Link from 'next/link'
 import LogoImg from '../assets/Logo.svg'
 import { globalStyles } from '../styles/global'
 import { Container, Header } from '../styles/pages/app'
@@ -10,7 +11,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <Image src={LogoImg} alt="" />
+        <Link href="/">
+          <Image src={LogoImg} alt="" />
+        </Link>
       </Header>
 
       <Component {...pageProps} />
